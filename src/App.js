7dc10c1b84256255d1de9fcb4630badf608955b3
UnJustice IsReal <github@harry.eu.org>
2024-01-05 11:51:55 -0500

    Added files

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+import {Navbar} from './components/navbar';
+import {Textform} from './components/textarea';
+import {Wordcounter} from './components/wordcounter';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Footer } from './components/footer';
+
+
+export const App =()=> {
+
+  return (
+<>
+<Router>
+        <Navbar title="Online Converter Tool" />
+        <div className='main-container'>
+  <Routes>
+  <Route path="/" element={<Textform heading="Enter the text"/>} />
+  <Route exact path='/' element={ <Wordcounter wcheading="Paste Text And Get Text Summary"/> } />
+          </Routes>
+          
+
+        </div>
+        <Footer/>
+    </Router>
+</>
+  )
+}
+
